33e4f98251233645ed8b59d148711c6ff1d65a21
EzequielGBC <ezequielcepeda93@gmail.com>
2026-09-19 15:35:40 -0300

    Creacion de los atributos de la clase Instructor y su Constructor

--- src/Instructor.java
+++ src/Instructor.java
@@ -1,4 +1,16 @@
 package PACKAGE_NAME;
 
 public class Instructor {
+    //Atributos en privado
+    private String nombre;
+    private String apellido;
+    private String matricula;
+
+    //Constructor
+    public Instructor(String nombre, String apellido, String matricula){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.matricula = matricula;  
+    }
+
 }
