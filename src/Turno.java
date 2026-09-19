2d965e69ab77494e864846310b0746df95b077a2
EzequielGBC <ezequielcepeda93@gmail.com>
2026-09-19 16:19:48 -0300

    Creamos la constante de cupos, los atributos y el constructor de la clase Turno

--- src/Turno.java
+++ src/Turno.java
@@ -1,4 +1,22 @@
 package PACKAGE_NAME;
 
 public class Turno {
+    //Constante 
+    public static final int CUPO_MAXIMO = 20;
+
+    //Atributos
+    private String fecha;
+    private String horario;
+    private Instructor instructor;
+    private Inscripcion[] incripciones;
+    private String catidadInscriptos;
+
+    //Constructor
+    public Turno(String fecha, String horario, Instructor instructor){
+        this.fecha = fecha;
+        this.horario = horario;
+        this.instructor = instructor;
+        this.inscripciones = new Incripcion[CUPO_MAXIMO];
+        this.cantidadInscriptos = 0;
+    }
 }
