aea9a24518c2e5a0130e243354c90d49981979bc
arevalitobautista <arevalitobautista@gmail.com>
2026-09-20 20:57:42 -0300

    Agrego getters y setters a Escuela y Tabla

--- src/Escuela.java
+++ src/Escuela.java
@@ -3,12 +3,37 @@ public class Escuela {
     private String direccion;
     private String telefono;
 
-    public Escuela(String nombre, String direccion, String telefono){
+    public Escuela(String nombre, String direccion, String telefono) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.telefono = telefono;
     }
-    public void descripcion() {
-        System.out.println("Escuela: " + nombre + " - Direccion: " + direccion + " - Telefono: " + telefono);
+
+    public Escuela(String nombre, String direccion) {
+        this(nombre, direccion, " Sin telefono");
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String descripcion() {
+        return "Escuela: " + nombre + " - Direccion: " + direccion + " - Telefono: " + telefono;
     }
 }
--- src/Tabla.java
+++ src/Tabla.java
@@ -5,13 +5,22 @@ public class Tabla {
 
     public Tabla(int codigo, double largo, String estado){
         this.codigo = codigo;
-        this.largo=largo;
-        this.estado=estado;
+        setLargo(largo);
     }
-    public void cambiarEstado(String nuevoEstado){
-        this.estado=nuevoEstado;
+    public int getCodigo(){
+        return codigo;
     }
-    public void descripcion() {
-        System.out.println("Tabla Nro: " + codigo + " - Largo: " + largo + "m - Estado: " + estado);
+    public double getLargo(){
+        return largo;
+    }
+    public void setLargo(double largo){
+        if(largo>= 5 && largo <= 10){
+            this.largo = largo;
+        }else{
+            System.out.println("Error: El largo de la tabla debe estar entre 5 y 10 pies");
+        }
+    }
+    public String descripcion() {
+        return "Tabla Nro: " + codigo + " - Largo: " + largo + "m - Estado: " + estado;
     }
 }
