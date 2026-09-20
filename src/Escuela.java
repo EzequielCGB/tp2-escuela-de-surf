c6a5e14b9d3a65ae0f80347bf1325e7b0decfafe
arevalitobautista <arevalitobautista@gmail.com>
2026-09-20 17:05:11 -0300

    Agrego atributos y constructores a Escuela y Tabla

--- src/Escuela.java
+++ src/Escuela.java
@@ -1,2 +1,14 @@
 public class Escuela {
+    private String nombre;
+    private String direccion;
+    private String telefono;
+
+    public Escuela(String nombre, String direccion, String telefono){
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+    public void descripcion() {
+        System.out.println("Escuela: " + nombre + " - Direccion: " + direccion + " - Telefono: " + telefono);
+    }
 }
--- src/Tabla.java
+++ src/Tabla.java
@@ -1,2 +1,17 @@
 public class Tabla {
+    private int codigo;
+    private double largo;
+    private String estado;
+
+    public Tabla(int codigo, double largo, String estado){
+        this.codigo = codigo;
+        this.largo=largo;
+        this.estado=estado;
+    }
+    public void cambiarEstado(String nuevoEstado){
+        this.estado=nuevoEstado;
+    }
+    public void descripcion() {
+        System.out.println("Tabla Nro: " + codigo + " - Largo: " + largo + "m - Estado: " + estado);
+    }
 }
